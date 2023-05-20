548329112434f2d6a44f6a2319d3ce44f3d2553b
Hugo Moreira <hugo.n4@gmail.com>
2023-05-19 19:07:12 -0700

    update

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log('Hello World')
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+app
+  .listen({
+    port: 3000,
+  })
+  .then(() => {
+    console.log('Server listening on port 3000')
+  })
